613607bad96fac684c19eb376afe376145e405ba
Antoni Kowalewski <kowalewski.antek@gmail.com>
2021-07-15 00:55:44 +0200

    Add propTypes and image adress

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import styles from './Hero.scss';
+import PropTypes from 'prop-types';
 
-const Hero = () => (
+const Hero = props => (
   <header className={styles.component}>
-    <h2 className={styles.title}>Things to do. Hero.</h2>
-    <img className={styles.image} src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png"></img>
+    <h2 className={styles.title}>{props.titleText}</h2>
+    <img className={styles.image} src={props.headerImage}></img>
   </header>
 );
+
+Hero.propTypes = {
+  titleText: PropTypes.node.isRequired,
+  headerImage: PropTypes.node.isRequired,
+};
 
 export default Hero;
