1ae2d3001437bfcc9d0cfb7d1838aedf9d129bbd
Antoni Kowalewski <kowalewski.antek@gmail.com>
2021-08-02 12:58:39 +0200

    Add second fail

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import './styles/global.scss';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
 
-const test = "1"
+const test = "2"
 
 ReactDOM.render(<App />, document.getElementById('app'));
